30437cb375ffc2d6453e892727ee72017231f32d
LeftLu-note <luizarodriguescardoso@usp.br>
2026-04-06 17:27:33 -0300

    Lista1 - Parte 4: Vetores e Memoria
    Leitura e escrita na memoria

---
+++ Graduação/Programação de Maquina/ArqOrq/lista1_41.asm
@@ -0,0 +1,89 @@
+# Parte 4 : Vetores e Memória
+# Leitura de Inteiro e armazenamento na memoria
+
+	.data
+	.align 0
+str_wcm: .asciz "Digite a quantidade de numéros que você gostaria de armazenar: "
+str_w2:  .asciz "Digite o número: "
+
+str_fim: .asciz "Os números foram armazenados na memória!"
+#debug
+str_err: .asciz "Erro na alocação de memória! "
+
+	.text
+	.align 2
+	.globl main
+main:
+	#imprime  qtd
+	addi a7, zero, 4
+	la a0, str_wcm
+	ecall
+	
+	#leitura de qtd
+	addi a7, zero, 5
+	ecall
+	
+	#salvando um copia pro contador
+	add s0, zero, a0
+	
+	#calculando a qtd de bytes
+	# a0 =  (s0 * 4) bytes
+	addi t0, zero, 4
+	mul a0, s0, t0
+	
+	#alocando a memoria
+	addi a7, zero, 9
+	ecall
+	
+	#em caso de erro na alocação, finaliza
+	beq a0, zero, erro
+	
+	#caso sucesso, percorre loop para guardar
+	#t1 =  primeiro endereço que o ponteiro vai mostrar
+	add t1, zero, a0
+loop:
+	beq s0, zero, fim
+	
+	#pede o numero
+	addi a7, zero, 4
+	la a0, str_w2
+	ecall
+	
+	#le o inteiro em a0
+	addi a7, zero, 5
+	ecall
+	
+	#escreve o conteudo de a0 na posicao offset+qtd
+	sw a0, 0(t1)
+	
+	#avança o ponteiro
+	addi t1, t1, 4
+	
+	#decrementa o contador
+	addi s0, s0, -1
+	
+	j loop
+	
+fim: 	
+	#carrega a msg de sucesso
+	addi a7, zero, 4
+	la a0, str_fim
+	ecall
+	
+	#finaliza
+	addi a7, zero, 10
+	ecall
+
+ erro:  
+	#carrega a msg de erro
+	addi a7, zero, 4
+	la a0, str_err
+	ecall
+	
+	#finaliza
+	addi a7, zero, 10
+	ecall
+	
+	
+	
+	
---
+++ Graduação/Programação de Maquina/ArqOrq/lista1_42.asm
@@ -0,0 +1,124 @@
+# Parte 4 : Vetores e Memória
+# Leitura de Inteiro e armazenamento na memoria
+# Recuperação dos dados
+
+	.data
+	.align 0
+str_wcm: .asciz "Digite a quantidade de numéros que você gostaria de armazenar: "
+str_w2:  .asciz "Digite o número: "
+str_val: .asciz "Número armazenado: "
+str_str: .asciz "\n"
+str_fim: .asciz "Os números foram armazenados e recuperados na memória!\n"
+#debug
+str_err: .asciz "Erro na alocação de memória! "
+
+	.text
+	.align 2
+	.globl main
+main:
+	#imprime  qtd
+	addi a7, zero, 4
+	la a0, str_wcm
+	ecall
+	
+	#leitura de qtd
+	addi a7, zero, 5
+	ecall
+	
+	#salvando um copia pro contador
+	add s0, zero, a0
+	
+	#calculando a qtd de bytes
+	# a0 =  (s0 * 4) bytes
+	addi t0, zero, 4
+	mul a0, s0, t0
+	
+	#alocando a memoria
+	addi a7, zero, 9
+	ecall
+	
+	#em caso de erro na alocação, finaliza
+	beq a0, zero, erro
+	
+	#caso sucesso, percorre loop para guardar
+	#t1 =  primeiro endereço que o ponteiro vai mostrar
+	add t1, zero, a0
+	add t2, zero, a0
+	
+loop:
+	beq s0, zero, recupera
+	
+	#pede o numero
+	addi a7, zero, 4
+	la a0, str_w2
+	ecall
+	
+	#le o inteiro em a0
+	addi a7, zero, 5
+	ecall
+	
+	#escreve o conteudo de a0 na posicao offset+qtd
+	sw a0, 0(t1)
+	
+	#avança o ponteiro
+	addi t1, t1, 4
+	
+	#decrementa o contador
+	addi s0, s0, -1
+	
+	j loop
+	
+recupera:
+	
+	beq t2, t1, fim
+	
+	#Imprime msg 
+	addi a7, zero, 4
+	la a0, str_val
+	ecall 
+	
+	#busca na memoria o dados e imprime na tela
+	#carrega da memoria o valor em offset(t2)
+	lw t3, 0(t2)
+	
+	#guarda em a0 o valor que sera imprimido
+	add a0, zero, t3
+	
+	#imprime na tela
+	addi a7, zero, 1
+	ecall
+	
+	#Imprime \n
+	addi a7, zero, 4
+	la a0, str_str
+	ecall 
+	
+	#avança na memoria
+	addi t2, t2, 4
+	
+	
+	j recupera
+	
+fim: 	
+	#carrega a msg de sucesso
+	addi a7, zero, 4
+	la a0, str_fim
+	ecall
+	
+	#finaliza
+	addi a7, zero, 10
+	ecall
+
+ erro:  
+	#carrega a msg de erro
+	addi a7, zero, 4
+	la a0, str_err
+	ecall
+	
+	#finaliza
+	addi a7, zero, 10
+	ecall
+	
+	
+	
+	
