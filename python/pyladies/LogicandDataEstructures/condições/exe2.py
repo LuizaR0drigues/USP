f37988487540466734d67b8475a7ecf1e91c671b
LeftLu-note <luizarodriguescardoso@usp.br>
2026-04-07 21:17:10 -0300

    Separando Pastas

--- python/pyladies/LogicandDataEstructures/condições/exe2.py
+++
@@ -1,7 +0,0 @@
-velocidade = int(input("Digite a velocidade do carro: \n"))
-print(velocidade)
-if velocidade > 80:
-    multa = (velocidade - 80) * 7
-    print(f'O valor da multa será: {multa}')
-else:
-    print('Parabens por respeitar as regras de trânsito :)')
