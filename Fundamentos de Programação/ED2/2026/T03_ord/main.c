644e82534f76be67f3eeea19a053ddc7d9660146
LeftLu-note <luizarodriguescardoso@usp.br>
2026-04-13 14:41:55 -0300

    Implementação do MergeSort

--- Fundamentos de Programação/ED2/2026/T03_ord/main.c
+++ Fundamentos de Programação/ED2/2026/T03_ord/main.c
@@ -20,149 +20,140 @@ typedef struct
 
 } Brinquedo;
 
-void swap(Brinquedo vetor[], int a, int b)
+void merge(Brinquedo vetor[], int inicio, int meio, int final)
 {
-    Brinquedo aux;
+    // definindo os limites dos intervalos dxe observacao
+    int firs_met = meio - inicio + 1;
+    int sec_met = final - meio;
+    int i, j, k;
 
-    aux = vetor[a];
-    vetor[a] = vetor[b];
-    vetor[b] = aux;
+    // criando vetores auxiliares
+    Brinquedo esquerda[firs_met], direita[sec_met];
+
+    // fazendo copias do vetor para cada metade - vetor/2 por iteração
+    // divide
+    for (i = 0; i < firs_met; i++)
+    {
+        // subvetor da esquerda
+        esquerda[i] = vetor[inicio + i];
+    }
+
+    for (j = 0; j < sec_met; j++)
+    {
+        // subvetor da direita
+        direita[j] = vetor[meio + j + 1];
+    }
+
+    i = 0, j = 0, k = inicio;
+    // ordenação das partes
+    while (i < firs_met && j < sec_met)
+    {
+        int flag = 0;
+        int criterioCor = strcmp(esquerda[i].cor, direita[j].cor);
+        if (criterioCor < 0)
+        {
+            flag = 1; // cor da esquerda é vem primeiro
+        }
+        else if (criterioCor == 0) // empate de cor
+        {
+            if (esquerda[i].comprimento < direita[j].comprimento)
+            {
+                flag = 1; // msm cor, porem comprimento menor
+            }
+            else if (esquerda[i].comprimento == direita[j].comprimento)
+            { // ordena por nota
+                if (esquerda[i].nota >= direita[j].nota)
+                {
+                    flag = 1; // só muda a nota que é maior
+                }
+            }
+        }
+        if (flag)
+        {
+            vetor[k] = esquerda[i];
+            i++;
+        }
+        else
+        {
+            vetor[k] = direita[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copia elems que sobraram
+    while (i < firs_met)
+    {
+        vetor[k] = esquerda[i];
+        i++;
+        k++;
+    }
+    while (j < sec_met)
+    {
+        vetor[k] = direita[j];
+        j++;
+        k++;
+    }
 }
 
-// ordenada por cor
-// apos, ordena por comprimento
-// se houver empate, ordena por nota
-
-int particiona(Brinquedo vet[], int pIni, int pFin)
-{
-    Brinquedo pivo;
-    pivo = vet[pIni];
-
-    int i = pIni + 1;
-    int j = pFin;
-    int aux = 0;
-    while (i <= j)
+void mergesort(Brinquedo vetor[], int inicio, int final)
+{ // para cada chamada que cumpra a condição
+    if (inicio < final)
     {
-        // ordena por cor
-        while (i <= pFin)//procura pelo maior
-        {
-            int criterioCor = strcmp(vet[i].cor, pivo.cor);
-            if (criterioCor < 0)
-            {
-                i++; // ordem alfabetica
-            }
-            else if (criterioCor == 0) // empate de cor
-            {
-                if (vet[i].comprimento < pivo.comprimento)
-                {
-                    i++;
-                }
-                else if (vet[i].comprimento == pivo.comprimento)
-                { // ordena por nota
-                    if (vet[i].nota > pivo.nota)
-                    {
-                        i++;
-                    }
-                    else
-                    {
-                        break; // maior que o pivo em tudo
-                    }
-                }
-                else
-                {
-                    break; // comprimento > pivo
-                }
-            }
-            else
-            {
-                break; // vem depois alfabeticamente
-            }
-        }
-        while (j >= i)//procura pelo menor
-        {
-            int criterioCor = strcmp(vet[j].cor, pivo.cor);
-            if (criterioCor > 0)
-            {
-                j--; // cor vem depois - à direita
-            }
-            else if (criterioCor == 0) // empate de cor
-            {
-                if (vet[j].comprimento > pivo.comprimento)
-                {
-                    j--; //cor igual, comprimento diff
-                }
-                else if (vet[j].comprimento == pivo.comprimento)
-                { // ordena por nota
-                    if (vet[j].nota < pivo.nota)
-                    {
-                        j--;//cor e comprimento iguais, usa criterio de nota
-                    }
-                    else
-                    {
-                        break; // nota menor ou igual que o pivo
-                    }
-                }
-                else
-                {
-                    break; // comprimento menor 
-                }
-            }
-            else
-            {
-                break; // vem antes alfabeticamente
-            }
-        }
-        if (i < j)
-        {
-            swap(vet, i, j);
-        }
-    }
-    swap(vet, j, pIni);
-    return j;
-}
+        // identifica o indice medio do vetor
+        int meio = (inicio) + (final - inicio) / 2;
 
-void quicksort(Brinquedo vetor[], int pIni, int pFin)
-{
-    if (pFin < +pIni)
-    {
-        return;
+        // divide, recuriva, o vetor em duas partes
+        // esquerda(init, meio)
+        mergesort(vetor, inicio, meio);
+        // direita(meio+1, final)
+        mergesort(vetor, meio + 1, final);
+
+        // aplica o merge
+        merge(vetor, inicio, meio, final);
     }
-    int meio = particiona(vetor, pIni, pFin);
-    quicksort(vetor, pIni, meio - 1);
-    quicksort(vetor, meio + 1, pFin);
 }
 
 int main()
 {
     char c;
-    int qtd_brinq;
-    scanf("%d", &qtd_brinq);
+    double qtd_brinq;
+    scanf("%lf", &qtd_brinq);
 
-    // criando um vetor de struct
-    Brinquedo *brinquedos;
-    brinquedos = (Brinquedo *)malloc(qtd_brinq * sizeof(Brinquedo));
-
-    int i = 0;
-    while (i < qtd_brinq)
+    if (qtd_brinq > 0 && qtd_brinq <= 1e6)
     {
-        // leitura dos dados e tranfere para a estrutura
-        scanf("%s %f %f", brinquedos[i].cor, &brinquedos[i].comprimento, &brinquedos[i].nota);
-        brinquedos[i].id = i;
-        // leitura de char
-        scanf("%c", &c);
-        i++;
-        // se for um caractere de final de instrução, termina o loop
-        if (c == '\n' || c == '\r')
+        // criando um vetor de struct
+        Brinquedo *brinquedos;
+        brinquedos = (Brinquedo *)malloc(qtd_brinq * sizeof(Brinquedo));
+
+        int i = 0;
+        while (i < qtd_brinq)
         {
-            break;
+            // leitura dos dados e tranfere para a estrutura
+            scanf("%s %f %f", brinquedos[i].cor, &brinquedos[i].comprimento, &brinquedos[i].nota);
+            brinquedos[i].id = i;
+            
+            // leitura de char
+            scanf("%c", &c);
+            i++;
+            // se for um caractere de final de instrução, termina o loop
+            if (c == '\n' || c == '\r')
+            {
+                break;
+            }
         }
-    }
-    quicksort(brinquedos, 0, qtd_brinq - 1);
+        mergesort(brinquedos, 0, qtd_brinq - 1);
 
-    for (int i = 0; i < qtd_brinq; i++)
+        for (int i = 0; i < qtd_brinq; i++)
+        {
+            printf("%d;", brinquedos[i].id);
+            // printf("%d %s %.2f %.2f\n", brinquedos[i].id, brinquedos[i].cor, brinquedos[i].comprimento, brinquedos[i].nota);
+        }
+        free(brinquedos);
+    }
+    else
     {
-        printf("%d;", brinquedos[i].id);
-        //printf("%d %s %.2f %.2f\n", brinquedos[i].id, brinquedos[i].cor, brinquedos[i].comprimento, brinquedos[i].nota);
+        return 0;
     }
     return 0;
 }
